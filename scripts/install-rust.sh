967b7e825b1387535c1442083536284a0f1eac99
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2023-04-03 10:36:22 +0200

    fix: install-rust.sh

--- scripts/install-rust.sh
+++ scripts/install-rust.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Get the current working directory
-CWD=$(pwd)
+PWD=$(pwd)
+
+export CARGO_HOME=$PWD/.cargo
+export RUSTUP_HOME=$PWD/.rustup
 
 # Install Rust and Cargo in the CWD
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain stable --no-modify-path --prefix="$CWD"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --profile minimal --default-toolchain stable --no-modify-path
 
 # Add Cargo to PATH
-export PATH="$CWD/bin:$PATH"
+export PATH=$PATH:$PWD/.cargo/bin
 
 # Install wasm-pack
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -f --prefix="$CWD/bin"
