a4b80817101989b5c37f3995e00bc626e7b31b2d
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2023-04-03 11:02:46 +0200

    fix: build script

--- scripts/build-rust.sh
+++ scripts/build-rust.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+# Get the current working directory
+PWD=$(pwd)
+
+# Check if the .cargo/bin directory exists
+if [ -d "$PWD/.cargo/bin" ]; then
+  # Add Cargo to PATH
+  export PATH="$PWD/.cargo/bin:$PATH"
+fi
+
 cargo --help
 wasm-pack --help
